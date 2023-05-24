1ee4cdc1517660e1a94a0c6d97349b1294f71a11
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-05-24 07:29:17 +0200

    Add components

--- vite-yu-gi-oh/src/components/AppComponent.vue
+++ vite-yu-gi-oh/src/components/AppComponent.vue
@@ -18,6 +18,28 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <main>
+        <div class="container">
+            <select name="" id="choose">
+                <option value="1">Mazzo Mostro</option>
+                <option value="2">Carte Magia</option>
+                <option value="3">Carte Trappola</option>
+            </select>
 
-<style lang="scss" scoped></style>
+        </div>
+
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    background-color: goldenrod;
+
+    #choose {
+        margin: 0.5rem;
+        padding: 0.5rem;
+        border-radius: 20px;
+    }
+}
+</style>
--- vite-yu-gi-oh/src/components/AppHeader.vue
+++ vite-yu-gi-oh/src/components/AppHeader.vue
@@ -18,6 +18,26 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <header>
+        <img src="../assets/Yu-Gi-Oh!_(Logo).jpg" alt="Logo" width="100" height="40">
+        <h1>Yu-Gi-Oh API</h1>
 
-<style lang="scss" scoped></style>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+
+header {
+    background-color: whitesmoke;
+    display: flex;
+    // align-items: flex-start;
+    padding: 1rem;
+
+    h1 {
+        margin-left: 20px;
+        font-size: 3rem;
+    }
+}
+</style>
