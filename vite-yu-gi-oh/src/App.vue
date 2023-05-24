f62a026a51b9785280b0ea9e86e050e5dfdef32b
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-05-24 07:01:05 +0200

    Init project

---
+++ vite-yu-gi-oh/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppComponent from './components/AppComponent.vue';
+import AppCard from './components/AppCard.vue';
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppComponent,
+    AppCard
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppComponent />
+  <AppCard />
+</template>
+
+<style lang="scss" scoped>
+@use './styles/general.scss' as *;
+@use './styles/partials/_mixins.scss' as *;
+@use './styles/partials/_variables.scss' as *;
+</style>
